a6a8707b6a2c5f923131463e029f05ecb1e22e5f
MC SERVER <mcserver@localhost.localdomain>
2019-05-31 09:18:54 -0700

    Does not work.

--- autopush.sh
+++ autopush.sh
@@ -1,4 +1,4 @@
 #autopushes to master branch
 git add -A
-git commit -m "$(curl -s http://whatthecommit.com/index.txt)"
+git commit -m "$(curl -s http://whatthecommit.com/index.txt)" #random text generator
 git push origin master
